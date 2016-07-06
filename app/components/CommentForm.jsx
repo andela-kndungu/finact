ac59fa5af90a302c1822c4e9fd9af0a5feb69775
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-06 14:39:00 +0300

    Clear on submit

--- app/components/CommentForm.jsx
+++ app/components/CommentForm.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 
-export default function () {
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="Your name" />
-        <input type="text" placeholder="Say something..." />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+class CommentForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      author: '',
+      text: '',
+    };
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleAuthorChange(event) {
+    this.setState({ author: event.target.value });
+  }
+
+  handleTextChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ author: '', text: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Your name"
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+          />
+          <input
+            type="text"
+            placeholder="Say something..."
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
+
+export default CommentForm;
 
