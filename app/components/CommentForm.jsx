7b9f5b199c431c3d9c955d415282ab298d05b516
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-06 12:56:44 +0300

    Build comment form

--- app/components/CommentForm.jsx
+++ app/components/CommentForm.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function () {
-  return (<h1>I am the CommentForm</h1>);
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Your name" />
+        <input type="text" placeholder="Say something..." />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
