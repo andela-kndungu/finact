f62709d73482c3ca5bbb70ce679bb4734164c905
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-07 23:51:06 +0300

    A hello world

---
+++ app/components/Editor.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Editor, EditorState } from 'draft-js';
+import { convertFromRaw, convertToRaw } from 'draft-js';
+
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
+  }
+
+	render() {
+		const ab = convertToRaw(contentState: ContentState);
+    const { editorState } = this.state;
+    return <Editor editorState={editorState} onChange={this.onChange} />;
+  }
+
+}
+export default MyEditor;
+
--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import CommentBox from './components/CommentBox.jsx';
+import Editor from './components/Editor.jsx';
 
 ReactDom.render(
-  <CommentBox url="http://finact.herokuapp.com/api/comments/" />,
+  <Editor />,
   document.getElementById('app')
 );
 
